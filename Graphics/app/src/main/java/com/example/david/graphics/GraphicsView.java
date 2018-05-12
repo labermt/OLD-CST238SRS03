5e98ddb4c5d42956c367d1ac6f4f41f26cb79a8c
David Russell <davidr0013@gmail.com>
2018-05-11 21:07:32 -0700

    partially complete

---
+++ Graphics/app/src/main/java/com/example/david/graphics/GraphicsView.java
@@ -0,0 +1,181 @@
+package com.example.david.graphics;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Random;
+
+import static android.view.MotionEvent.ACTION_DOWN;
+import static android.view.MotionEvent.ACTION_UP;
+import static java.lang.Math.abs;
+import static java.lang.Math.round;
+
+public class GraphicsView extends View
+{
+    private Paint graphicsPaint;
+    private List<Point> points;
+    private List<Point> calculatedPoints;
+    private Random randGen;
+
+    public float fraction;
+    public int iterations;
+
+    public boolean firstTime;
+    private boolean isPressed;
+    private int x, y;
+
+
+    public GraphicsView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        initGraphics();
+
+        setOnTouchListener( new View.OnTouchListener() {
+            Calendar timeStamp;
+            public boolean onTouch(View v, MotionEvent event) {
+                Calendar now = Calendar.getInstance();
+                if(longEnough(timeStamp, now)) {
+                    timeStamp = now;
+                    if (event.getActionMasked() == ACTION_DOWN) {
+                        manageTouchDown(event);
+                    } else if (event.getActionMasked() == ACTION_UP) {
+                        manageTouchUp(event);
+                        performClick();
+                    }
+                }
+                return true;
+            }
+        });
+    }
+
+    private void addPoint(float x, float y) {
+        points.add( new Point(round(x), round(y)) );
+    }
+
+
+    private void initGraphics() {
+        graphicsPaint = new Paint();
+        graphicsPaint.setStyle(Paint.Style.STROKE);
+        graphicsPaint.setStrokeCap(Paint.Cap.ROUND);
+        graphicsPaint.setColor(Color.BLUE);
+        points = new ArrayList<>();
+        calculatedPoints = new ArrayList<>();
+        fraction = 0.5f;
+        iterations = 5000;
+        randGen = new Random( Calendar.getInstance().getTimeInMillis() );
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        graphicsPaint.setStrokeWidth(4);
+        for(Point point : points)
+        {
+            canvas.drawCircle(point.x, point.y, 4, graphicsPaint);
+        }
+        graphicsPaint.setStrokeWidth(2);
+        for(Point point : calculatedPoints)
+        {
+            canvas.drawPoint(point.x, point.y, graphicsPaint);
+        }
+    }
+
+    public void clearScreen() {
+        points.clear();
+        calculatedPoints.clear();
+        invalidate();
+    }
+
+    public void createArt() {
+        if(points.size() < 2) return;
+        Point currentPoint = points.get( randGen.nextInt(points.size()) );
+        for(int i = 0; i < iterations; i++)
+        {
+            currentPoint = calculatePoint(currentPoint);
+            calculatedPoints.add( currentPoint );
+        }
+        invalidate();
+    }
+
+    public void setFraction(int percent) {
+        fraction = (1.0f + percent) / 100;
+    }
+    public String getFraction(){
+        return String.valueOf(round(fraction*100))+"%";
+    }
+    public void setIteration(int iteration){
+        iterations = iteration;
+    }
+    public boolean back(){
+        if(!calculatedPoints.isEmpty()){
+            calculatedPoints.clear();
+            invalidate();
+            return true;
+        }
+        else if(!points.isEmpty()){
+            points.remove(points.size() - 1);
+            invalidate();
+            return true;
+        }
+        else return false;
+    }
+    public void setPointColor(int color){
+        graphicsPaint.setColor(color);
+        invalidate();
+    }
+
+    private Point calculatePoint(Point currentPoint){
+        float currentFraction = 1 - fraction;
+        Point randomPoint = points.get( randGen.nextInt(points.size()) );
+        float x = (randomPoint.x * fraction + currentPoint.x * currentFraction);
+        float y = (randomPoint.y * fraction + currentPoint.y * currentFraction);
+        return new Point(round(x), round(y));
+    }
+
+    private void manageTouchUp(MotionEvent event) {
+        int currentX = (int)event.getX();
+        int xDifference = currentX - x;
+        int currentY = (int)event.getY();
+        int yDifference = currentY - y;
+        if( abs(xDifference) < 30 && abs(yDifference) < 30){
+            addPoint(currentX, currentY);
+        }
+        else {
+            clearScreen();
+            /*
+            if(!calculatedPoints.isEmpty()){
+                calculatedPoints.clear();
+            }
+            else points.clear();
+            */
+        }
+        isPressed = false;
+        invalidate();
+    }
+    private void manageTouchDown(MotionEvent event) {
+        if(!isPressed){
+            x = (int)event.getX();
+            y = (int)event.getY();
+            isPressed = true;
+        }
+    }
+
+    private boolean longEnough(Calendar timeStamp, Calendar now) {
+        if (timeStamp == null) {
+            timeStamp = Calendar.getInstance();
+            return true;
+        }
+        return now.getTimeInMillis() > timeStamp.getTimeInMillis() + 200;
+    }
+}
---
+++ Graphics/app/src/main/java/com/example/david/graphics/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.david.graphics;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import com.skydoves.colorpickerpreference.ColorEnvelope;
+import com.skydoves.colorpickerpreference.ColorListener;
+import com.skydoves.colorpickerpreference.ColorPickerView;
+
+
+public class MainActivity extends AppCompatActivity
+{
+    private GraphicsView graphics;
+    private float fractionValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        graphics = (GraphicsView) findViewById(R.id.graphics_view);
+
+        ((Button)findViewById(R.id.runButton)).setOnClickListener(new OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                graphics.createArt();
+            }
+        });
+
+
+        SeekBar bar = (SeekBar)findViewById(R.id.fractionBar);
+        bar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                graphics.setFraction(progress);
+                ((TextView)findViewById(R.id.fractionValueLabel)).setText(graphics.getFraction());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                graphics.invalidate();
+            }
+        });
+        SeekBar itterBar = (SeekBar)findViewById(R.id.iterationBar);
+        itterBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                graphics.setIteration(progress);
+                ((TextView)findViewById(R.id.iterationValueLabel)).setText(String.valueOf(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {  }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {  }
+        });
+
+/*
+        //https://medium.com/@skydoves/how-to-implement-color-picker-in-android-61d8be348683
+        ((ColorPickerView)findViewById(R.id.colorPickerView)).setColorListener(new ColorListener() {
+            @Override
+            public void onColorSelected(ColorEnvelope colorEnvelope) {
+                graphics.setPointColor(colorEnvelope.getColor());
+            }
+        });
+*/
+    }
+
+    @Override
+    public void onBackPressed(){
+        if(!graphics.back()) super.onBackPressed();
+    }
+
+
+
+} //end MainActivity class
